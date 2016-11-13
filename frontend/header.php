6c4a3da29e99e5159016cfbbce23bd62cd49198d
mkrajnak <krajnakmatto@gmail.com>
2016-11-13 01:36:00 +0100

    Login screen with simple front page

--- frontend/header.php
+++ frontend/header.php
@@ -3,7 +3,25 @@
 <head>
     <meta charset='UTF-8'>
     <link rel='stylesheet' type='text/css' href='../style/main.css'/>
-    <title><?php echo $title ?></title>
+    <header>
+        <div class="nav">
+            <ul>
+                <li class="visits"><a class="active" href="#">Návštevy</a></li>
+                <li class="patients"><a href="#">Pacienti</a></li>
+                <li class="drugs"><a href="#">Lieky</a></li>
+                <li class="insurance"><a href="#">Poisťovne</a></li>
+                <li class="bills"><a href="#">Faktúry</a></li>
+                <li class="employee"><a href="#">Zamestnanec</a></li>
+            </ul>
+        </div>
+    </header>
 </head>
 
 <body>
+
+</body>
+<footer>
+    created for ISS 2016 ©
+    <a href="mailto:vkondula@gmail.com"> vkondula@gmail.com</a>
+    <a href="mailto:krajnakmatto@gmail.com"> krajnakmatto@gmail.com</a>
+</footer>
--- frontend/log_form.php
+++ frontend/log_form.php
@@ -1,11 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='UTF-8'>
+    <link rel='stylesheet' type='text/css' href='../style/main.css'/>
+    <title>Prihlásenie</title>
+    <header>
+        Prihlásenie
+    </header>
+</head>
+
+<body>
 <form action="../backend/log_page.php" method="post">
     <div class="container">
-        <label><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="email" required>
-
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password">
-
-        <button type="submit">Login</button>
+        <div class="center">
+            <label><b>Email</b></label>
+        </div>
+        <div class="center">
+            <input type="text" placeholder="Enter Email" name="email" required>
+        </div>
+        <div class="center">
+            <label><b>Password</b></label>
+        </div>
+        <div class="center">
+            <input type="password" placeholder="Enter Password" name="password" required>
+        </div>
+        <div class="center">
+            <button class="loginbtn" type="submit">Login</button>
+        </div>
     </div>
 </form>
+</body>
+<footer>
+    created for ISS 2016 ©
+    <a href="mailto:vkondula@gmail.com"> vkondula@gmail.com</a>
+    <a href="mailto:krajnakmatto@gmail.com"> krajnakmatto@gmail.com</a>
+</footer>
