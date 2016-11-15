01d5d7994d351d30619830c3eefea40f1fce652f
mkrajnak <krajnakmatto@gmail.com>
2016-11-15 22:41:43 +0100

    mostly css corrections

--- frontend/header.php
+++ frontend/header.php
@@ -30,7 +30,8 @@ if (!$user->has_password() and $title != "Změna hesla"){
                 <li class="insurance"><a <?php if($title == 'Pojišťovny') echo 'class="active"'?> href="insurance.php">Pojišťovny</a></li>
                 <li class="bills"><a <?php if($title == 'Faktury') echo 'class="active"'?> href="bills.php">Faktury</a></li>
                 <li class="employee"><a <?php if($title == 'Zaměstnanci') echo 'class="active"'?> href="employee.php">Zaměstnanci</a></li>
-                <li class="user"><a>Martin</a>
+                <li class="user">
+                    <a>Martin</a>
                     <ul id="submenu">
                         <a class="submenu_item" href="#">Change password</a>
                         <a class="submenu_item" href="#">Logout</a>
